5022eb61bd3dee27887165304c1fb2f773aff656
Aswad <aswadsupu5@gmail.com>
2025-08-17 14:37:56 +0800

    update fitur

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <div className="container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-10 py-4">
           {/* Logo */}
           <div className="flex items-center">
-            <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-[#1C352D]">
+            <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-primary">
               LOGO
             </span>
           </div>
@@ -13,30 +13,30 @@ export default function Header() {
           {/* Navigation */}
           <nav className="flex items-center gap-6 sm:gap-10 lg:gap-16">
             {/* Menu links - hidden on small screens */}
-            <div className="hidden lg:flex items-center gap-6 lg:gap-10 font-semibold">
+            <div className="hidden md:flex items-center gap-6 lg:gap-10 font-semibold">
               <a
                 href="#"
-                className="text-base sm:text-lg lg:text-xl hover:text-[#A6B28B] transition"
+                className="text-base sm:text-lg lg:text-xl  transition"
               >
                 Home
               </a>
               <a
                 href="#"
-                className="text-base sm:text-lg lg:text-xl hover:text-[#A6B28B] transition"
+                className="text-base sm:text-lg lg:text-xl  transition"
               >
                 About
               </a>
               <a
                 href="#"
-                className="text-base sm:text-lg lg:text-xl hover:text-[#A6B28B] transition"
+                className="text-base sm:text-lg lg:text-xl  transition"
               >
                 Details
               </a>
             </div>
 
             {/* Button */}
-            <button className="px-4 py-2 rounded-full border-2  text-primary hover:bg-[#1C352D] hover:text-[#A6B28B] transition-all">
-              <span className="text-xs font-bold">Mulai Aplikasi</span>
+            <button className="hidden md:flex lg:px-9 lg:py-4 px-6 py-3 rounded-full border-2  text-primary hover:bg-primary hover:text-secondary transition-all ">
+              <span className="text-sm font-bold">Mulai Aplikasi</span>
             </button>
           </nav>
         </div>
