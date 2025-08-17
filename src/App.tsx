e03b6e9c98d72e66449dbadcb041e9d225dfb5cc
Aswad <aswadsupu5@gmail.com>
2025-08-17 12:08:03 +0800

    first

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <>
-      <div className="w-7xl h-screen mx-auto overflow-x-hidden text-[#1C352D] ">
+      <div className="md:w-7xl w-full h-screen mx-auto overflow-x-hidden text-[#1C352D] ">
         <Header />
       </div>
     </>
